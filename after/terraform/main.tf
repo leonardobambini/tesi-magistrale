1bf54f04d797e3ee3525a9bdcdc7411c55032056
leonardobambini <bambini.leonardo1@gmails.com>
2024-05-19 20:55:10 +0200

    commented modules to run action

--- after/terraform/main.tf
+++ after/terraform/main.tf
@@ -20,40 +20,40 @@ provider "aws" {
   # secret_key = var.secret_key
 }
 
-module "ecommerce" {
-  source = "./modules/ecommerce"
-  alb_sgs = [module.networking.ecommerce_sg_id]
-  security_groups_dev = [module.networking.ecommerce_dev_sg_id]
-  alb_subnets = [module.networking.ecommerce_prd_subnet1_id, module.networking.ecommerce_prd_subnet2_id]
-  vpc_id = module.networking.ecommerce_prd_vpc_id
-  subnet_prd_1_id = module.networking.ecommerce_prd_subnet1_id
-  subnet_prd_2_id = module.networking.ecommerce_prd_subnet2_id
-  subnet_dev_id = module.networking.ecommerce_dev_subnet_id
-}
+# module "ecommerce" {
+#   source = "./modules/ecommerce"
+#   alb_sgs = [module.networking.ecommerce_sg_id]
+#   security_groups_dev = [module.networking.ecommerce_dev_sg_id]
+#   alb_subnets = [module.networking.ecommerce_prd_subnet1_id, module.networking.ecommerce_prd_subnet2_id]
+#   vpc_id = module.networking.ecommerce_prd_vpc_id
+#   subnet_prd_1_id = module.networking.ecommerce_prd_subnet1_id
+#   subnet_prd_2_id = module.networking.ecommerce_prd_subnet2_id
+#   subnet_dev_id = module.networking.ecommerce_dev_subnet_id
+# }
 
-module "networking" {
-  source = "./modules/networking"
-}
+# module "networking" {
+#   source = "./modules/networking"
+# }
 
-module "db" {
-  source = "./modules/db"
-  db_username = var.db_username
-  db_password = var.db_password
-}
+# module "db" {
+#   source = "./modules/db"
+#   db_username = var.db_username
+#   db_password = var.db_password
+# }
 
-module "blog" {
-  source = "./modules/blog"
-  vpc_id = module.networking.blog_prd_vpc_id
-  subnet_prd_id = module.networking.blog_prd_subnet_id
-  subnet_dev_id = module.networking.blog_dev_subnet_id
-  security_groups = [module.networking.blog_sg_id]
-  security_groups_dev = [module.networking.blog_dev_sg_id]
-}
+# module "blog" {
+#   source = "./modules/blog"
+#   vpc_id = module.networking.blog_prd_vpc_id
+#   subnet_prd_id = module.networking.blog_prd_subnet_id
+#   subnet_dev_id = module.networking.blog_dev_subnet_id
+#   security_groups = [module.networking.blog_sg_id]
+#   security_groups_dev = [module.networking.blog_dev_sg_id]
+# }
 
-module "finops" {
-  source = "./modules/finops"
-  alert_mail_recvr = var.alert_mail_recvr
-}
+# module "finops" {
+#   source = "./modules/finops"
+#   alert_mail_recvr = var.alert_mail_recvr
+# }
 
 module "s3" {
   source = "./modules/s3"
